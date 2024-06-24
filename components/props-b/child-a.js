7a438b175b72d01b2528522b18ac65f150ca03a6
leechinghua <leechenghua@gmail.com>
2024-06-24 14:10:28 +0800

    props屬性

--- components/props-b/child-a.js
+++ components/props-b/child-a.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function ChildA(pData) {
+export default function ChildA({ dataFromChild }) {
   return (
     <>
       <h3>ChildA</h3>
-      <p>來自父母元件的資料: {pData}</p>
+      <p>來自子女B的資料: {dataFromChild}</p>
     </>
   )
 }
