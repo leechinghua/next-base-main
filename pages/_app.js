f87bfbc2a2e0a5675ae6e707d96b35f43445d9ec
leechinghua <leechenghua@gmail.com>
2024-07-09 13:37:56 +0800

    0709上午

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
 // import '@/styles/globals.css'
 // import '@/styles/product-table.css'
+import { useEffect } from 'react'
 import { AuthProvider } from '@/hooks/use-auth'
 import { CartProvider } from '@/hooks/use-cart'
+import '@/styles/globals.scss'
 
 export default function MyApp({ Component, pageProps }) {
   // 使用自訂在頁面層級的版面(layout)
   const getLayout = Component.getLayout || ((page) => page)
+
+  useEffect(() => {
+    // 要document物件出現後才能導入 bootstrap的js函式庫
+    import('bootstrap/dist/js/bootstrap')
+  }, [])
 
   return (
     <CartProvider>
